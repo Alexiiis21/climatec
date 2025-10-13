acd37dc3807dc3269d734eb1e229eb2fc44ed7b7
Alexiiis21 <acardenas22@alumnos.uaq.mx>
2025-10-12 19:38:56 -0600

    workflow fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,17 +9,22 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
-];
+// Condición basada en NODE_ENV
+const disableEslint = process.env.DISABLE_ESLINT_IN_PROD === 'true' && process.env.NODE_ENV === 'production';
+
+const eslintConfig = disableEslint
+  ? [] // Desactiva ESLint
+  : [
+      ...compat.extends("next/core-web-vitals", "next/typescript"),
+      {
+        ignores: [
+          "node_modules/**",
+          ".next/**",
+          "out/**",
+          "build/**",
+          "next-env.d.ts",
+        ],
+      },
+    ];
 
 export default eslintConfig;
